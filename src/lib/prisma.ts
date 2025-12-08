43f1c98af87247f7c71ccfaa70b00305eacefc52
weslleymirandadev <contato@weslleymiranda.dev>
2025-12-08 06:23:57 -0300

    fix(postgres): removed postgres lib

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,11 +1,18 @@
-import { PrismaClient } from '@/generated/prisma/client';
-import { PrismaPg } from '@prisma/adapter-pg';
-import { Pool } from 'pg';
+import { PrismaClient } from "@/generated/prisma/client";
 
-const connectionString = process.env.DATABASE_URL;
-const pool = new Pool({ connectionString });
-const adapter = new PrismaPg(pool);
+const globalForPrisma = globalThis as unknown as {
+  prisma?: PrismaClient;
+};
 
-let prisma = new PrismaClient({ adapter });
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    accelerateUrl: process.env.DATABASE_URL!,
+    log: process.env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
+  });
+
+if (process.env.NODE_ENV === "development") {
+  globalForPrisma.prisma = prisma;
+}
 
 export default prisma;
