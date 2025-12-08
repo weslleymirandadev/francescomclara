70e1d42935fb8920c15c21c15abf889bfc442ed9
weslleymirandadev <contato@weslleymiranda.dev>
2025-12-08 19:43:38 -0300

    feat(auth): added auth provider

---
+++ src/app/providers.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import { ReactNode } from "react";
+import { AuthProvider } from "@/context/AuthProvider";
+import { CartProvider } from "@/context/CartContext";
+
+interface ProvidersProps {
+  children: ReactNode;
+}
+
+export function Providers({ children }: ProvidersProps) {
+  return (
+    <AuthProvider>
+      <CartProvider>{children}</CartProvider>
+    </AuthProvider>
+  );
+}
