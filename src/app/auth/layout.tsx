f1dd9988081bbc04ddaf4131d9f559afa47e064f
weslleymirandadev <contato@weslleymiranda.dev>
2026-01-04 23:42:41 -0300

    feat(metadata): added metadata

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -1,13 +1,20 @@
-import { AuthProvider } from "@/context/AuthProvider";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Autenticação - Frances Com Clara",
+  description: "Faça login ou registre-se em Frances Com Clara",
+};
 
 export default function AuthLayout({
-    children,
-}: Readonly<{
-    children: React.ReactNode;
-}>) {
-    return (
-		<AuthProvider>
-			{children}
-		</AuthProvider>
-    );
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-primary">
+      <div className="max-w-md w-full space-y-8">
+        {children}
+      </div>
+    </div>
+  );
 }
